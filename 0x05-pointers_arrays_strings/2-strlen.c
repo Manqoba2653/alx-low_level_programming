a0b12663191e34bb87a3f1139735396deaf693b7
Manqoba2653 <manqobak143@gmail.com>
2023-06-26 13:48:31 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+size_t _strlen(const char *str)
+{
+	size_t length  = 0;
+
+	while (*str++)
+	lenght++;
+	return (length);
+}
