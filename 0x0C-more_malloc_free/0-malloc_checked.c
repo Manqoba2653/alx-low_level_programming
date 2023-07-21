1bac3af9215bd01b460402b5df958344885c8ea4
Manqoba2653 <manqobak143@gmail.com>
2023-07-21 13:53:41 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,15 +7,12 @@
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 	
 	ptr = malloc(b);
-	
 	if (ptr == NULL)
 		exit(98);
-	
 	return (ptr);
 }
